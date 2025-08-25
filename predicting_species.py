e6e2bb4501ce35246223911d5a4aecb3f9c20e40
Codeair <24hussainm@rbunagpur.in>
2025-08-26 01:25:02 +0530

    Update predicting_species.py

--- predicting_species.py
+++ predicting_species.py
@@ -1,22 +1,22 @@
-# %%
 import joblib
 import pandas as pd
-model = joblib.load('Iris_DecisionTreeModel.pkl')
-val1 = float(input("Sepal Length in cm: "))
-val2 = float(input("Sepal Width in cm: "))
-val3 = float(input("Petal Length in cm: "))
-val4 = float(input("Petal Width in cm: "))
 
-sample_df = pd.DataFrame([[val1, val2, val3, val4]],columns=['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)'])
+def predict_species(model_name,sl,sw,pl,pw):
+
+    if(model_name == 'decision_tree'):
+        model = joblib.load('Iris_DecisionTreeModel.pkl')
+    else:
+        model = joblib.load('Iris_KNN.pkl')
+    
+    
+    sample_df = pd.DataFrame([[sl, sw, pl, pw]],columns=['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)'])
 
 
-y_pred = model.predict(sample_df)
+    y_pred = model.predict(sample_df)
 
 
-iris_target = ['setosa' , 'veriscolor' , 'virginica'] 
-print('\n\nPredicted Iris Species: '+ iris_target[y_pred[0]]+'\n\n')
-
-input("Press Enter to close the program...")
+    iris_target = ['setosa' , 'versicolor' , 'virginica'] 
+    return iris_target[y_pred[0]]
 
 
 
